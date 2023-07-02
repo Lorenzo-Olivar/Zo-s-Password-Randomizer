ee9a003936111c477967192bc23fc1a15d494968
Lorenzo-Olivar <131014446+Lorenzo-Olivar@users.noreply.github.com>
2023-07-02 15:31:44 -0700

    added an alert and prompt in the js file

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  // Alert notifies the user of what to write down
+  alert('Pls specify and write down which modifiers you want to include in your password.');
+  // Prompt creates an array of the users desired password modifiers
+  let inputFirst = prompt('uc - Uppercase | lc - Lowercase | num - Numbers | spc - Special Characters', 'Please list each desired password modifier seperated by a one space');
+  let passCriteria = inputFirst.split(" ");
+  // console.log(passCriteria); The Array worked and logged in the user's modifiers into an array
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -12,3 +19,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Test Area 
+
+
